e2aac91e0d2f6389221ba237f1fd7805c9c98496
Rhaulsal <rhaulsal2007@gmail.com>
2023-11-23 20:17:54 -0300

    envio 02

---
+++ app/src/main/java/br/com/etecia/myapp/AdapterFilmes.java
@@ -0,0 +1,9 @@
+package br.com.etecia.myapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterFilmes extends RecyclerView.Adapter<AdapterFilmes.ViewHolder> {
+
+
+    public class  ViewHolder
+}
